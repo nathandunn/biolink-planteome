95d67c313278360ed94e09bf640f5d203ffe52d6
Nathan Dunn <ndunn@me.com>
2017-06-28 14:53:40 +0200

    updated test script

--- test_script.sh
+++ test_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-planemo t associations.xml bioentity-associations.xml bulk-operations.xml disease-associations.xml gene-associations.xml genotype-associations.xml  get-object.xml  phenotype-gene-associations.xml search-operations.xml substance-associations.xml variant-associations.xml
+planemo t associations.xml bioentity-associations.xml bulk-operations.xml disease-associations.xml gene-associations.xml  get-object.xml  
 
 
